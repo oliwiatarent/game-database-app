86e434c91da9f1cc7a0a14e8d5211957100c90d7
Oliwia Tarent <oliwia.tarent@student.put.poznan.pl>
2026-01-03 14:55:28 +0100

    Dodawanie i usuwanie pozostałych elementów

--- games/urls.py
+++ games/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('games/', views.game_list, name='games'),
     path("game/<int:id>/", views.game, name="game"),
-    path("game_form/", views.game_form, name="game_form")
+    path("add_game/", views.add_game, name="add_game"),
+    path("add/<str:type>", views.add, name="add"),
+    path("delete/<str:type>", views.delete, name="delete")
 ]
--- games/views.py
+++ games/views.py
@@ -32,6 +32,8 @@ def game(request, id):
 
     game = {}
     columns = []
+    genres_list = []
+    platform_list = []
 
     with oracledb.connect(user=username, password=password, dsn=cs) as connection:
         with connection.cursor() as cursor:
@@ -44,76 +46,81 @@ def game(request, id):
             for column in cursor:
                 columns.append(column[0].lower())
 
-            cursor.execute(f"SELECT * FROM {username}.gry WHERE id = {id}")
+            sql = f"SELECT * FROM {username}.gry WHERE id = :1"
+            cursor.execute(sql, (id, ))
 
-            for item in cursor:
-                for i in range(len(item)):
+            game_row = cursor.fetchone()
+            for i in range(len(game_row)):
 
-                    if columns[i] == "ograniczenie_wiekowe":
-                        attribute = 'img/age_ratings/' + str(item[i]) + '.png'
-                    elif columns[i] == "data_wydania":
-                        attribute = item[i].date
-                    else:
-                        attribute = item[i]
+                if columns[i] == "ograniczenie_wiekowe":
+                    attribute = 'img/age_ratings/' + str(game_row[i]) + '.png'
+                elif columns[i] == "data_wydania":
+                    attribute = game_row[i].date
+                elif columns[i] == "franczyza" and game_row[i] is None:
+                    attribute = 'Brak'
+                else:
+                    attribute = game_row[i]
 
-                    game[columns[i]] = attribute
+                game[columns[i]] = attribute
 
 
             # Przetwarzanie dewelopera
-            cursor.execute(f"""
-                SELECT nazwa 
-                FROM {username}.gry g INNER JOIN deweloperzy d ON d.id = g.deweloper
-                WHERE g.id = {id}
-            """)
+            if (game["deweloper"] is None):
+                game["deweloper"] = 'Brak'
+            else:
+                sql = f"""
+                    SELECT nazwa 
+                    FROM {username}.gry g INNER JOIN deweloperzy d ON d.id = g.deweloper
+                    WHERE g.id = :1
+                """
+                cursor.execute(sql, (id,))
 
-            for item in cursor:
-                game["deweloper"] = item[0]
-
+                game["deweloper"] = cursor.fetchone()[0]
 
             # Przetwarzanie gatunków
-            cursor.execute(f"""
+            sql = f"""
                 SELECT nazwa_gatunku 
                 FROM Gry_Gatunki
-                WHERE ID_Gry = {id}
-            """)
+                WHERE ID_Gry = :1
+            """
+            cursor.execute(sql, (id, ))
 
-            genres_list = []
             for item in cursor:
                 genres_list.append(item[0])
 
             if len(genres_list) == 0:
-                game["gatunki"] = "None"
+                game["gatunki"] = "Brak"
             else:
                 game["gatunki"] = ', '.join(genres_list)
 
 
             # Przetwarzanie platform
-            cursor.execute(f"""
+            sql = f"""
                 SELECT nazwa_platformy 
                 FROM Gry_Platformy
-                WHERE ID_Gry = {id}
-            """)
+                WHERE ID_Gry = :1
+            """
+            cursor.execute(sql, (id, ))
 
-            platform_list = []
             for item in cursor:
                 platform_list.append(item[0])
 
             if len(platform_list) == 0:
-                game["platformy"] = "None"
+                game["platformy"] = "Brak"
             else:
                 game["platformy"] = ', '.join(platform_list)
 
 
             # Przetwarzanie gry podstawowej dla dodatków
             if game["dodatek"] == 1:
-                cursor.execute(f"""
+                sql = f"""
                     SELECT tytul 
                     FROM Gry
-                    WHERE ID = {game["gra_podstawowa"]}
-                """)
+                    WHERE ID = :1
+                """
+                cursor.execute(sql, (game["gra_podstawowa"], ))
 
-                for item in cursor:
-                    game["tytul_gry_podstawowej"] = item[0]
+                game["tytul_gry_podstawowej"] = cursor.fetchone()[0]
 
 
     return render(request, "game.html", {
@@ -121,7 +128,7 @@ def game(request, id):
     })
 
 
-def game_form(request):
+def add_game(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         releaseDate = request.POST.get('releaseDate')
@@ -143,8 +150,7 @@ def game_form(request):
                     dlc = 1
                     cursor.execute('SELECT id FROM Gry WHERE tytul = :1', (dlcBaseGame, ))
 
-                    for item in cursor:
-                        dlcBaseGame = item[0]
+                    dlcBaseGame = cursor.fetchone()[0]
                 else:
                     dlc = 0
                     dlcBaseGame = None
@@ -233,10 +239,90 @@ def game_form(request):
                 })
 
 
-    return render(request, "game_form.html", {
+    return render(request, "add_game_form.html", {
         "developers": developers,
         "franchises": franchises,
         "baseGames": baseGames,
         "platforms": platforms,
         "genres": genres
+    })
+
+
+def add(request, type):
+
+    if request.method == 'POST':
+        value = request.POST.get('value')
+
+        with oracledb.connect(user=username, password=password, dsn=cs) as connection:
+            with connection.cursor() as cursor:
+
+                if type == 'gatunek':
+                    sql = "INSERT INTO Gatunki VALUES (:1)"
+                if type == 'platforma':
+                    sql = "INSERT INTO Platformy VALUES (:1)"
+                if type == 'franczyza':
+                    sql = "INSERT INTO Franczyzy VALUES (:1)"
+                if type == 'deweloper':
+                    sql = "INSERT INTO Deweloperzy VALUES (null, :1)"
+
+                cursor.execute(sql, (value, ))
+                connection.commit()
+
+        return redirect('games')
+
+    return render(request, "add_form.html", {
+        "type": type
+    })
+
+
+def delete(request, type):
+
+    if request.method == 'POST':
+        attribute = request.POST.get('attribute')
+
+        print(attribute)
+
+        with oracledb.connect(user=username, password=password, dsn=cs) as connection:
+            with connection.cursor() as cursor:
+
+                if type == 'gatunek':
+                    sql = "DELETE FROM Gatunki WHERE nazwa = :1"
+                if type == 'platforma':
+                    sql = "DELETE FROM Platformy WHERE nazwa = :1"
+                if type == 'franczyza':
+                    sql = "DELETE FROM Franczyzy WHERE nazwa = :1"
+                if type == 'deweloper':
+                    sql = "DELETE FROM Deweloperzy WHERE nazwa = :1"
+                if type == 'gra':
+                    sql = "DELETE FROM Gry WHERE tytul = :1"
+
+                cursor.execute(sql, (attribute, ))
+                connection.commit()
+
+        return redirect('games')
+
+
+    attributes = []
+
+    with oracledb.connect(user=username, password=password, dsn=cs) as connection:
+        with connection.cursor() as cursor:
+            if type == 'gatunek':
+                sql = "SELECT nazwa FROM Gatunki"
+            if type == 'platforma':
+                sql = "SELECT nazwa FROM Platformy"
+            if type == 'franczyza':
+                sql = "SELECT nazwa FROM Franczyzy WHERE nazwa NOT LIKE 'Brak'"
+            if type == 'deweloper':
+                sql = "SELECT nazwa FROM Deweloperzy WHERE nazwa NOT LIKE 'Brak'"
+            if type == 'gra':
+                sql = "SELECT tytul FROM Gry"
+
+            cursor.execute(sql)
+
+            for item in cursor:
+                attributes.append(item[0])
+
+    return render(request, "delete_form.html", {
+        "type": type,
+        "attributes": attributes
     })
